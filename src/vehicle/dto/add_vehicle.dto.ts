a28d3692e2bb8dc917ad0ddb9b398884b796ef61
gsdhillon94 <gsdhillon94@gmail.com>
2023-10-15 00:51:02 +1100

    adding validation to add vehicle API

--- src/vehicle/dto/add_vehicle.dto.ts
+++ src/vehicle/dto/add_vehicle.dto.ts
@@ -1,8 +1,24 @@
+import { IsInt, IsNotEmpty, IsNumber, IsString, MinLength } from "class-validator";
+
 export class AddVehicleDto{
+    @IsString()
+    @MinLength(2, { message: 'vehicleName must have atleast 2 characters.' })
+    @IsNotEmpty()
     vehicleName: string;
+    @IsString()
+    @MinLength(10, { message: 'vehicleDescription must have atleast 10 characters.' })
+    @IsNotEmpty()
     vehicleDescription: string;
+    @IsInt()
+    @IsNotEmpty()
     vehiclePriceWeekly: number;
+    @IsInt()
+    @IsNotEmpty()
     vehicleTypeId: number;
+    @IsInt()
+    @IsNotEmpty()
     vehicleUseTypeId: number;
+    @IsInt()
+    @IsNotEmpty()
     userId: number
 }
